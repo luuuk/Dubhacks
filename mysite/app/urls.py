ad07ba9be85f04273c81e8a7c100aa49915e760d
Luke LeVasseur <luke.levasseur16@gmail.com>
2019-10-12 18:16:43 -0700

    routing

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -4,6 +4,10 @@ from . import views
 urlpatterns = [
     # ex: /
     path('', views.index, name='index'),
+    # /resources
     path('resources', views.resources, name='resources'),
     path('file_a_report', views.questions, name='Info'),
+    path('sign_in', views.emailver, name='Verify Email'),
+    path('action', views.assaultaction(), name='What Happened'),
+    path('attributes', views.attributecollection(), name='Perpetrator Details'),
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -11,5 +11,18 @@ def resources(request):
     return render(request, 'app/resources.html')
 
 
-def questions():
+def questions(request):
     return render(request, 'app/question.html')
+
+
+def emailver(request):
+    return render(request, 'app/emailVer.html')
+
+
+def assaultaction(request):
+    return render(request, 'app/assaultAction.html')
+
+
+def attributecollection(request):
+    return render(request, 'app/attrCol.html')
+
