8058cf863778ead833e7d9cd498870fe39adc580
Luke LeVasseur <luke.levasseur16@gmail.com>
2019-10-12 17:41:17 -0700

    landing page

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -2,12 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # ex: /test/
+    # ex: /
     path('', views.index, name='index'),
-    # ex: /test/5/
-    path('<int:question_id>/', views.detail, name='detail'),
-    # ex: /test/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
-    # ex: /test/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('/resources', views.index, name='index'),
 ]
