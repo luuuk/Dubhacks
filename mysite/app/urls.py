c0cca3d980151ad541ef2ec517a151f59217b3bd
Luke LeVasseur <luke.levasseur16@gmail.com>
2019-10-12 17:55:10 -0700

    basic html

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     # ex: /
     path('', views.index, name='index'),
-    path('/resources', views.index, name='index'),
+    path('resources', views.resources, name='resources'),
+    path('file_a_report', views.questions, name='Info'),
 ]
--- mysite/app/views.py
+++ mysite/app/views.py
@@ -5,3 +5,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'app/index.html')
+
+
+def resources(request):
+    return render(request, 'app/resources.html')
+
+
+def questions():
+    return render(request, 'app/question.html')
