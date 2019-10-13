fc0b1cb1fde6e93cf211810c64859be44fc20c35
Serena Gilani <serenagilani@comcast.net>
2019-10-13 04:35:10 -0700

    fixed sexual harassment options

--- mysite/app/urls.py
+++ mysite/app/urls.py
@@ -15,6 +15,7 @@ urlpatterns = [
     path('attrMatch', views.attrmatch, name='Perpetrator Match'),
     path('advisorConnect', views.advisorconnect, name='Connect with an Advisor'),
     path('login', views.login, name='Login'),
-    path('match', views.match, name='Match')
+    path('match', views.match, name='Match'),
+    path('attrCol', views.attrCol, name="AttrCol"),
 
 ]
